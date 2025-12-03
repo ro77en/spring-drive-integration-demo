843abc4bc84f56274fb49970b63c9a4d987f29ae
ro77en <lucas.mdantas@icloud.com>
2025-12-03 17:15:10 -0300

    initial commit

---
+++ src/main/java/com/ro77en/drive_integration_demo/DriveIntegrationDemoApplication.java
@@ -0,0 +1,13 @@
+package com.ro77en.drive_integration_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DriveIntegrationDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DriveIntegrationDemoApplication.class, args);
+	}
+
+}
